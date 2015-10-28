8169b6d63e786b21cabf44966ae375457b88986f
paomian <qtang@leancloud.rocks>
2015-10-28 15:20:54 +0800

    (Update) change jetty version

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.eclipse.jetty/jetty-server "9.3.5.v20151012"]
+                 [org.eclipse.jetty/jetty-server "9.2.10.v20150310"]
                  [ring/ring-servlet "1.4.0"]
-                 [org.eclipse.jetty/jetty-servlet "9.3.5.v20151012"]
-                 [org.eclipse.jetty/jetty-jmx "9.3.5.v20151012"]
+                 [org.eclipse.jetty/jetty-servlet "9.2.10.v20150310"]
+                 [org.eclipse.jetty/jetty-jmx "9.2.10.v20150310"]
                  [hystrix-event-stream-clj "0.1.3"
                   :exclusions [com.netflix.hystrix/hystrix-clj]]
                  [com.netflix.hystrix/hystrix-clj "1.4.11"]])
