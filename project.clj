157245d3e4c8a3669747d1281f4608eed0b1fd11
paomian <qtang@leancloud.rocks>
2018-06-20 14:36:55 +0800

    :package: update hystrix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cn.leancloud/ring-jetty-hystrix-adapter "0.2.4"
+(defproject cn.leancloud/ring-jetty-hystrix-adapter "0.2.5"
   :description "Setup a Hystrix (https://github.com/Netflix/Hystrix) event stream with jetty for clojure."
   :url "http://github.com/killme2008/ring-jetty-hystrix-adapter"
   :license {:name "Eclipse Public License"
@@ -8,5 +8,5 @@
                  [ring/ring-servlet "1.4.0"]
                  [org.eclipse.jetty/jetty-servlet "9.2.10.v20150310"]
                  [org.eclipse.jetty/jetty-jmx "9.2.10.v20150310"]
-                 [com.netflix.hystrix/hystrix-clj "1.5.2"]
-                 [com.netflix.hystrix/hystrix-metrics-event-stream "1.5.2"]])
+                 [com.netflix.hystrix/hystrix-clj "1.5.11"]
+                 [com.netflix.hystrix/hystrix-metrics-event-stream "1.5.11"]])
